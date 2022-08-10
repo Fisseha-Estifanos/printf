a1f3f9984cec3825abd2d5efa1aa7cbc505b2405
Daniel <danieloladele7@gmail.com>
2022-08-10 21:23:33 +0200

    Helper function update

--- helper_function.c
+++ helper_function.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * rev_string - reverses a string in place
+ * rev_string - reverses a string in place.
  * @s: string to reverse
  * Return: A pointer to a character
  */
@@ -44,8 +44,8 @@ void write_base(char *str)
 
 /**
  * base_len - Calculates the length for an octal number
- * @num: The number for which the length is being calculated
- * @base: Base to be calculated by
+ * @num: The number for which the length is being calculated.
+ * @base: Base to be calculated by.
  * Return: An integer representing the length of a number
  */
 unsigned int base_len(unsigned int num, int base)
@@ -53,9 +53,7 @@ unsigned int base_len(unsigned int num, int base)
 	unsigned int i;
 
 	for (i = 0; num > 0; i++)
-	{
 		num = num / base;
-	}
 	return (i);
 }
 
